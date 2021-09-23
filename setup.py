f2724b51b726cad29bf09050d20d005550df82dc
Bastien Roques <bastien.roques1@gmail.com>
2021-09-23 10:50:34 +0200

    0.9.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as fp:
 
 setup(
     name="django-scheduler",
-    version="0.9.6",
+    version="0.9.7",
     description="A calendaring app for Django.",
     long_description=long_description,
     long_description_content_type="text/markdown",
